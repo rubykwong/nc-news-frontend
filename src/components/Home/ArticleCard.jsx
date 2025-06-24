0fbf28e8d3be663ee12e748e7c8512514e717c52
Ruby Kwong <ruby.kwong@btinternet.com>
2025-06-24 13:16:48 +0100

    add basic styling to article cards and article list

--- src/components/Home/ArticleCard.jsx
+++ src/components/Home/ArticleCard.jsx
@@ -3,13 +3,24 @@ import { Link } from 'react-router-dom';
 function ArticleCard({article}) {
     return (
         <Link to={`/${article.article_id}`} className="article-card">
-            <h3>{article.title}</h3>
-            <p>{article.created_at}</p>
-            <p>{article.topic}</p>
-            <p>{article.author}</p>
             <img src={article.article_img_url} alt={`article image for ${article.title}`}/>
-            <p>Votes:{`${article.votes}`}</p>
-            <p>Comments:{`${article.comment_count}`}</p>
+            <h3>{article.title}</h3>
+            <div className="card-footer">
+            <p> 
+                {new Date(article.created_at).toLocaleString('en-GB', {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false,
+                })}
+            </p>
+            <p>{article.author}</p>
+            <p>Votes: {`${article.votes}`}</p>
+            <p>Comments: {`${article.comment_count}`}</p>
+            </div>
+            <p>{article.topic}</p>
         </Link>
     )
 }
