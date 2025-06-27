3f9298e9f1d88eb2f901eb0d03ca7e956b52296f
Ruby Kwong <ruby.kwong@btinternet.com>
2025-06-27 16:29:20 +0100

    change error page heading from h1 to h2

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 function ErrorPage () {
     return (
         <section className="error-page">
-            <h1>404 - Page Not Found</h1>
+            <h2>404 - Page Not Found</h2>
             <p>Hmm...looks like that page doesn't exist.</p>
             <Link to="/" className="back-home-button">Back to Home </Link>
             </section>
