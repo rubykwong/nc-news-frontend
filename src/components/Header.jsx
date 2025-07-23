2f80046a4a03920c5fa4eba4b019b5dde45b8ee3
Ruby Kwong <ruby.kwong@btinternet.com>
2025-07-23 10:54:04 +0100

    refactor: convert header into link to home and add onHover styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Header () {
     return (
     <header>
+        <Link to={`/`}>
         <h1>Northcoders News</h1>
+        </Link> 
         <p>The North's favourite news outlet</p>
     </header>
     )
